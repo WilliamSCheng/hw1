e5471bbf1940f6daea50a26dcf52849b2fcdfaa0
William Cheng <william.cheng@berkeley.edu>
2014-10-04 00:18:04 -0700

    Finished hw

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   #get '/about', to: 'CONTROLLER#FUNCTION'
   #get '/user/new', to: 'CONTROLLER#FUNCTION', as: 'users'
 
-
+  root 'users#index'
+  get '/user/new', to: 'users#new', as: 'users'
 
   #Edit above this line
   get '/user/:id', to: 'users#show', as: 'user'
